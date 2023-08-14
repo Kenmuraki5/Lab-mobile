7b0a2876cb49eb86f243a8f4b3ef05b38ba394e1
Kenmuraki5 <89627339+Kenmuraki5@users.noreply.github.com>
2023-08-15 00:47:42 +0700

    lab 4

---
+++ lab-4/screens/MealDetailScreen.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { View, Text, Button, StyleSheet, FlatList } from "react-native";
+import { MEALS } from "../data/dummy-data";
+
+const MealDetailScreen = ({route, navigation}) => {
+  // เขียนโค้ดเพิ่ม เพื่อดึงข้อมูลเกี่ยวกับเมนูอาหารที่ผู้ใช้เลือกเอาไว้
+
+  return (
+    <View style={styles.screen}>
+      {/* <Text>The Meal Detail Screen!</Text> */}
+      <Text>Dish: {route.params.categoryTitle}</Text>
+      <FlatList
+        data={route.params.step}
+        renderItem={({item, index})=>{return <Text style={styles.text}>steps:{index+1}{item}</Text>}}
+      />
+
+      <Button
+        title="Go Back to Categories"
+        onPress={() => {
+          // เขียนโค้ดเพิ่ม
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    margin:30
+  },
+  text:{
+    margin:20
+  }
+});
+
+export default MealDetailScreen;
