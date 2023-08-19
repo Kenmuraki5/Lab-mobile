0909aa0180113a91a9b4bbb6008101ec02f0b080
Kenmuraki5 <89627339+Kenmuraki5@users.noreply.github.com>
2023-08-20 01:47:49 +0700

    update

--- lab-5/screens/CategoryMealsScreen.js
+++ lab-5/screens/CategoryMealsScreen.js
@@ -9,44 +9,11 @@ import {
 } from "react-native";
 import { CATEGORIES, MEALS } from "../data/dummy-data";
 import MealItem from "../components/MealItem";
-
+import MealList from "../components/MealList"
 const CategoryMealsScreen = ({route, navigation}) => {
-  const renderMealItem = (itemData) => {
-    return (
-      <MealItem
-        title={itemData.item.title}
-        duration={itemData.item.duration}
-        complexity={itemData.item.complexity}
-        affordability={itemData.item.affordability}
-        image={itemData.item.imageUrl}
-        onSelectMeal={() => {
-          // เขียนโค้ดเพิ่ม
-          console.log(itemData.item)
-          navigation.navigate("MealDetailScreen", {categoryTitle:itemData.item.title, step:itemData.item.steps, mealid:itemData.item.id})
-        }}
-      />
-
-      // ส่วนนี้ <View>...</View> ใช้เพื่อการทดลอง และให้คอมเมนต์โค้ดส่วนนี้และเรียกใช้ <MealItem> ข้างบนแทน
-      // <View style={{ height: 50, width: "40%" }}>
-      //   <Text>{console.log(itemData.item.title)}</Text>
-      // </View>
-    );
-  };
-
-  const catId = route.params.id
-
-
-  const displayedMeals = MEALS.filter(
-    (meal) => meal.categoryIds.indexOf(catId) >= 0
-  );
-
   return (
     <View style={styles.screen}>
-      <FlatList
-        style={{ width: "100%" }}
-        data={displayedMeals}
-        renderItem={renderMealItem}
-      />
+      <MealList listData={MEALS} navigation={navigation}/>
     </View>
 
     // ส่วนนี้ <View>...</View>ใช้เพื่อการทดลอง และให้คอมเมนต์โค้ดส่วนนี้และเรียกใช้ <FlatList> ข้างบนแทน
